a1ba21b0a0cf5081e9df3f77f0c047f1ee309a50
linaite <lina.dirmaite@gmail.com>
2020-06-18 11:32:59 +0300

    Pirmas page:logo-link

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include_once 'app/php/array.php';
+include_once 'app/php/function.php';
+
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<section class="container-fluid">
+    <div class="row vh-100 d-flex justify-content-center align-items-center">
+        <div class="col-6 h-50 bg-warning d-flex flex-column justify-content-center align-items-center">
+            <div>
+                <a class="h-100 d-flex justify-content-center align-items-center align-items-center"
+                   href="app/Views/home.php">
+                    <img class="w-25" src="https://paslaugos-img.dgn.lt/gallery_5_1910695/img.jpg" alt="Logo">
+                </a>
+                <h3 class="text-secondary text-center">Kviečiame atvykti!</h3>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="assets/js/jQuery.js"></script>
+<script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
